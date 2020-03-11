1b47e2062470b24dc68d70093bf67584a252b3e1
Cole Hille <colehille@gmail.com>
2020-03-11 01:31:41 -0400

    Replaced charecterController with RigidBody2D for collision purposes, remade movement script to work with RigidBody2D, and added collision for tilemap borders and field obsticles

--- Assets/movementLion.cs
+++ Assets/movementLion.cs
@@ -2,43 +2,74 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class movementLion : MonoBehaviour
-{
-    CharacterController characterController;
-    SpriteRenderer mySpriteRenderer;
-    public float speed = 6.0f;
-    private Vector3 moveDirection = Vector3.zero;
-    // Start is called before the first frame update
-    void Start()
-    {
-        characterController = GetComponent<CharacterController>();
-        mySpriteRenderer = GetComponent<SpriteRenderer>();
+public class movementLion : MonoBehaviour {
 
-    }
+	public float speed;
+	private Rigidbody2D charecterMovement;
 
-    // Update is called once per frame
-    void Update()
-    {
-        moveDirection = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
-        moveDirection *= speed;
-
-        characterController.Move(moveDirection * Time.deltaTime);//move controller
-
-        bool facingright = false;
-        if (Input.GetKeyDown(KeyCode.A))
-        {
-            //flip sprite
-            mySpriteRenderer.flipX = false;
-            facingright = false;
-        }
-        if (Input.GetKeyDown(KeyCode.D))
-        {
-            // flip the sprite
-            mySpriteRenderer.flipX = true;
-            facingright = true;
-        }
-
-
-    }
-
+	void Start() {
+		charecterMovement = GetComponent<Rigidbody2D>();
+	}
+	
+	void FixedUpdate() {
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVerticle = Input.GetAxis("Vertical");
+		Vector2 charecterMovementVector = new Vector2(moveHorizontal, moveVerticle);
+		charecterMovement.AddForce(charecterMovementVector * speed);
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//{
+//    CharacterController characterController;
+//    SpriteRenderer mySpriteRenderer;
+//    public float speed = 6.0f;
+//    private Vector3 moveDirection = Vector3.zero;
+//    // Start is called before the first frame update
+//    void Start()
+//    {
+//        characterController = GetComponent<CharacterController>();
+//        mySpriteRenderer = GetComponent<SpriteRenderer>();
+
+//    }
+
+//    // Update is called once per frame
+//    void Update()
+//    {
+//        moveDirection = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+//        moveDirection *= speed;
+
+//        characterController.Move(moveDirection * Time.deltaTime);//move controller
+
+//        bool facingright = false;
+//        if (Input.GetKeyDown(KeyCode.A))
+//        {
+//            //flip sprite
+//            mySpriteRenderer.flipX = false;
+//            facingright = false;
+//        }
+//        if (Input.GetKeyDown(KeyCode.D))
+//        {
+//            // flip the sprite
+//            mySpriteRenderer.flipX = true;
+//            facingright = true;
+//        }
+
+
+//    }
+
+//}
